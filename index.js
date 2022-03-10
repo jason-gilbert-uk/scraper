cd69a2f93064b75996438eb3447f37a1595b1cba
Jason Gilbert <jasongilbertuk@hotmail.com>
2022-03-10 16:39:13 +0000

    latest

--- index.js
+++ index.js
@@ -8,14 +8,247 @@ const cheerio = require('cheerio')
 var g_config=[];
 var g_indexToProcess = 0;
 var articles = [];
-const PROMOTION_TYPE = {
-    NONE: "None",
-    MEAL_DEAL: "Meal Deal",
-    ANY_X_FOR_Y: "Any x for y",
-    PER_KG: "Per Kg",
-    CHEAPEST_FREE: "Cheapest Free",
-    CLUBCARD_PRICE: "Clubcard Price",
-    MULTIBUY_PERCENT: "Buy x save y%"
+
+const promotionType = {
+    PERCENT_REDUCTION: 'percentage reduction',
+    CHEAPEST_PRODUCT_FREE: 'cheapest product free',
+    ANY_X_FOR_Y: 'any x for y',
+    REDUCED_TO_CLEAR: 'reduced to clear',
+    MEAL_DEAL: 'meal deal',
+    X_FOR_Y: 'x for y',
+    CLUBCARD_PRICE: 'clubcard price',
+    BOOK_CLUB: 'book club',
+    NO_OFFER: 'no offer',
+    UNRECOGNISED: "unrecognised"
+}
+
+function logDetails(product) {
+    //console.log(product);
+    console.log('promotion: ',product.productPromotionText);
+    console.log('promition type: ',product.promotionType);
+    console.log('quantity to achieve discount: ',product.quantityToAchieveDiscount); 
+    console.log('original price: ',product.price);
+    console.log('percentage discount: ',product.percentageDiscount)
+    console.log('discounted price: ',product.discountedPrice)
+}
+
+
+function processPercentOffer(product) {
+    //Buy 2 or more Save 25% Clubcard Price
+    product.promotionType = promotionType.PERCENT_REDUCTION;
+    const words = product.productPromotionText.split(' ');
+    product.quantityToAchieveDiscount = parseInt(words[1]); 
+    product.percentageDiscount = parseInt(words[5])
+    product.discountedPrice = (parseFloat(product.price) * ((100-product.percentageDiscount)/100));
+}
+
+function processCheapestProductFreeOffer(product) {
+    //Any 3 for 2 Clubcard Price - Cheapest Product Free - Selected Vegetables 80g - 800g
+    product.promotionType = promotionType.CHEAPEST_PRODUCT_FREE;
+    const words = product.productPromotionText.split(' ');
+    product.quantityToAchieveDiscount = parseInt(words[1]);
+    const y = parseInt(words[3]);
+    product.percentageDiscount = ((1.00 - (y/product.quantityToAchieveDiscount))*100);
+    product.discountedPrice = (parseFloat(product.price)*((100-product.percentageDiscount)/100));
+  
+}
+
+function processReducedToClearOffer(product) {
+    //Reduced to Clear Was £11.00 Now £7.50
+    product.promotionType = promotionType.REDUCED_TO_CLEAR;
+
+    const words = product.productPromotionText.split(' ');
+    words[4] = words[4].slice(1);   // Remove the £
+    words[6] = words[6].slice(1)    // Remove the £
+    product.price = parseFloat(words[4])
+    product.discountedPrice = parseFloat(words[6])
+    product.quantityToAchieveDiscount = 1;
+    product.percentageDiscount = ((1.00-(product.discountedPrice / product.price))*100)
+}
+
+function processAnyXforYOffer(product) {
+    //Any 2 for £1.30 Clubcard Price - Selected Lindt Premium Single Chocolates 35g Or 38g
+    product.promotionType = promotionType.ANY_X_FOR_Y;
+    
+    const words = product.productPromotionText.split(' ');
+    if (words[3].indexOf('£') !== -1) {
+        words[3] = words[3].slice(1);
+    } else if (words[3].indexOf('p')!== -1) {
+        words[3] = words[3].slice(0,words[3].length()-1)
+    } else {
+        console.log('error encountered in processAnyXforYOffer')
+        console.log(words)
+    }
+    product.quantityToAchieveDiscount = parseInt(words[1]);
+    totalCost = parseInt(words[3]);
+    product.percentageDiscount = ((1.00-((totalCost/product.quantityToAchieveDiscount)/product.price))*100);
+    product.discountedPrice = product.price*((100-product.percentageDiscount)/100)
+}
+
+function processXforYOffer(product) {
+    //4 for £3 or 8 for £5 Clubcard Price - Heinz Soup 400g
+    product.promotionType = promotionType.X_FOR_Y;
+
+    const words = product.productPromotionText.split(' ');
+    if (words[2].indexOf('£') !== -1) {
+        words[2] = words[2].slice(1);
+        
+    } else if (words[2].indexOf('p')!== -1) {
+        words[2] = words[2].slice(0,words[2].length-1)
+        words[2] = "0." +words[2]
+        
+    } else {
+        console.log('error')
+        console.log(words)
+    }
+    product.quantityToAchieveDiscount = parseInt(words[0]);
+    const totalPrice = parseInt(words[2]);
+    product.discountedPrice = totalPrice/product.quantityToAchieveDiscount;
+    product.percentageDiscount = (1.00-((product.discountedPrice/parseFloat(product.price))));
+}
+function processMealDealOffer(product){
+    product.promotionType = promotionType.MEAL_DEAL;
+
+    //£3 Meal Deal Clubcard Price £3.50 Meal Deal Regular Price - Selected Drink, Snack, Wrap, Sandwich, Roll, Pasty Or Salad//
+    //Currently we ignore meal deal offers.
+    product.percentageDiscount = 0;
+    product.quantityToAchieveDiscount = 1;
+    product.discountedPrice = product.price;
+}
+
+function processTescoBookClubOffer(product){
+    product.promotionType = promotionType.BOOK_CLUB;
+    
+
+    //TESCO BOOK CLUB
+    //Currently we ignore Tesco Book Club entries.
+    product.percentageDiscount = 0;
+    product.quantityToAchieveDiscount = 1;
+    product.discountedPrice = product.price;
+}
+
+function processClubcardPriceOffer(product){
+    //£2.00 Clubcard Price
+    product.promotionType = promotionType.CLUBCARD_PRICE;
+
+    const words = product.productPromotionText.split(' ');
+    if (words[0].indexOf('£') !== -1) {
+        words[0] = words[0].slice(1);
+    } else if (words[0].indexOf('p')!== -1) {
+        words[0] = words[0].slice(0,words[0].length-1)
+        words[0] = "0." +words[0]
+        
+    } else {
+        console.log('error')
+        console.log(words)
+    }
+    const x = parseFloat(words[0]);
+    product.percentageDiscount = (1.00-((parseFloat(words[0]/parseFloat(product.price)))));
+    product.quantityToAchieveDiscount = 1;
+    product.discountedPrice = parseFloat(words[0]);
+
+}
+
+function processNoOffer(product) {
+    product.promotionType = promotionType.NO_OFFER;
+    product.percentageDiscount = 0;
+    product.quantityToAchieveDiscount = 1;
+    product.discountedPrice = product.price;
+
+
+}
+
+function processUnrecognisedOffer(product) {
+    product.promotionType = promotionType.UNRECOGNISED;
+    product.percentageDiscount = 0;
+    product.quantityToAchieveDiscount = 1;
+    product.discountedPrice = product.price;
+
+    console.log('error: unrecognised offer type')
+    console.log(product);
+}
+
+
+
+
+function categorise(product) {
+    var category = promotionType.UNRECOGNISED;
+    var promotionText = product.productPromotionText;
+
+    if (product.isAvailable == false) {
+        //If product is not available, price is not given. Without the price, any promotion
+        //discounts can not be calculated. Therefore we set all to default values below.
+        
+    }
+    else if (promotionText==="") {
+        category =  promotionType.NO_OFFER;
+    }
+    else if (promotionText.indexOf("%") !== -1) {
+        category =  promotionType.PERCENT_REDUCTION;
+    }
+    else if (promotionText.indexOf("Cheapest Product Free") !== -1) {
+        category =  promotionType.CHEAPEST_PRODUCT_FREE;
+    } 
+    else if (promotionText.indexOf("Any") !== -1) {
+        category =  promotionType.ANY_X_FOR_Y;
+    }
+    else if (promotionText.indexOf("Reduced to Clear Was") !== -1) {
+        category =  promotionType.REDUCED_TO_CLEAR;
+    } 
+    else if (promotionText.indexOf("Meal Deal") !== -1) {
+        category =  promotionType.MEAL_DEAL;
+    }
+    else if (promotionText.indexOf("for") !== -1) {
+        category =  promotionType.X_FOR_Y;  
+    }
+    else if (promotionText.indexOf("Clubcard Price") !== -1) {
+        category =  promotionType.CLUBCARD_PRICE;  
+    }
+    else if (promotionText.indexOf("TESCO BOOK CLUB") !== -1) {
+        category =  promotionType.BOOK_CLUB;       
+    }
+    else {
+        category = promotionType.UNRECOGNISED;
+    }
+
+    return category;
+}
+
+function processCategory(product) {
+    product.category = categorise(product);
+    switch (product.category) {
+        case promotionType.PERCENT_REDUCTION:
+            processPercentOffer(product);
+            break;
+        case promotionType.CHEAPEST_PRODUCT_FREE:
+            processCheapestProductFreeOffer(product);
+            break;
+        case promotionType.ANY_X_FOR_Y:
+            processAnyXforYOffer(product);
+            break;
+        case promotionType.REDUCED_TO_CLEAR:
+            processReducedToClearOffer(product);
+            break;
+        case promotionType.MEAL_DEAL:
+            processMealDealOffer(product);
+            break;
+        case promotionType.X_FOR_Y:
+            processXforYOffer(product);
+            break;
+        case promotionType.CLUBCARD_PRICE:
+            processClubcardPriceOffer(product)
+            break;
+        case promotionType.BOOK_CLUB:
+            processTescoBookClubOffer(product);
+            break;
+        case promotionType.NO_OFFER:
+            processNoOffer(product);
+            break;
+        case promotionType.UNRECOGNISED:
+        default:
+            processUnrecognisedOffer(product)
+            break;
+    }
 }
 
 function getConfigIndexToProcess() {
@@ -85,7 +318,7 @@ async function processNextEntry() {
             var url = "https://www.tesco.com" + urlsub;
             var productId = url.substr(url.lastIndexOf('/') + 1);
             var imageUrl
-            var price =  parseFloat("0.00").toFixed(2);
+            var price =  parseFloat("0.00");
             if (price === 'NaN') {
                 console.log(url)
                 console.log('price is not a number')
@@ -97,10 +330,7 @@ async function processNextEntry() {
             var ProductPromotionDate = ''
             var ProductPromotionStart = ''
             var ProductPromotionEnd = ''
-            var clubcardPrice = price;
-            var purchaseNumber
-            var purchasePrice
-            var promotionType = PROMOTION_TYPE.NONE;
+            var promotionType = promotionType.NONE;
 
             const imageBlock = $(this).find('.product-image__container')
             imageUrl = $(imageBlock.find('img')).attr('src');
@@ -116,13 +346,17 @@ async function processNextEntry() {
                     "isAvailable" : isAvailable,
                     "price": price,
                     "aldiPriceMatch" : AldiPriceMatch,
-                    "hasPromotion": hasPromotion,
-                    "promotionType": promotionType,
                     "productPromotionText": ProductPromotionText,
                     "productPromotionDate": ProductPromotionDate,
                     "productPromotionStart": ProductPromotionStart,
                     "productPromotionEnd" : ProductPromotionEnd,
-                    "clubcardPrice": clubcardPrice};
+                    "quantityToAchieveDiscount" : 1,
+                    "pecentageDiscount": 0,
+                    "discountedPrice" : price
+                }
+                    const totalPrice = parseInt(words[2]);
+                    product.discountedPrice = totalPrice/product.quantityToAchieveDiscount;
+                    product.percentageDiscount = (1.00-((product.discountedPrice/parseFloat(product.price))));;
                 articles.push(article);
                 return true;            //skip to next item in each itteration.
             } 
@@ -143,104 +377,12 @@ async function processNextEntry() {
             ProductPromotionDate = ProductPromotionDate.slice(0,ProductPromotionDate.length/2);
             var showDeal = true;
            
-            var temp = ProductPromotionText.indexOf("%");
-            if (temp == -1) {
-                var temp = ProductPromotionText.indexOf("Cheapest Product Free");
-                if (temp == -1)
-                {
-                    temp = ProductPromotionText.indexOf("per kg Clubcard Price")
-                    if (temp == -1) {
-                        temp = ProductPromotionText.indexOf("Meal Deal");
-                        if (temp == -1){
-                            temp = ProductPromotionText.indexOf("Any ");
-                            if (temp == -1)
-                            {
-                                temp = ProductPromotionText.indexOf(" for");
-                                if (temp == -1 ) {
-                                    temp = ProductPromotionText.indexOf("Clubcard Price");
-                                    if (temp == -1) {
-                                        promotionType = PROMOTION_TYPE.NONE;
-                                        clubcardPrice = price;
-                                    } else if (temp==4) {
-                                        promotionType = PROMOTION_TYPE.CLUBCARD_PRICE;
-                                        clubcardPrice = parseFloat("0."+ ProductPromotionText.slice(0,2)).toFixed(2);
-                                    }else {
-                                        promotionType = PROMOTION_TYPE.CLUBCARD_PRICE;
-                                        clubcardPrice = parseFloat(ProductPromotionText.slice(1,temp-1)).toFixed(2);
-                                    }
-                                }
-                                else {
-                                    //3 for 2 without 'any'
-                                    promotionType = PROMOTION_TYPE.ANY_X_FOR_Y;
-                                    clubcardPrice = price;
-                                    temp = ProductPromotionText.indexOf("Clubcard Price");
-                                    purchaseNumber = ProductPromotionText.substring(0,1);
-                                    purchasePrice = ProductPromotionText.slice(7,temp-1);
-                                    clubcardPrice = (parseFloat(purchasePrice) / parseFloat(purchaseNumber)).toFixed(2);
-
-                                }
-                            } else {
-                                //Any 10 for £6.50 Clubcard Price - Selected Heinz Or Hipp Baby Food 160g - 200g
-                                promotionType = PROMOTION_TYPE.ANY_X_FOR_Y;
-                                clubcardPrice = price;
-                                temp = ProductPromotionText.indexOf("Clubcard Price");
-                                var countStart = 4;
-                                var countEnd = ProductPromotionText.indexOf("for"); 
-                                purchaseNumber = ProductPromotionText.substring(countStart,countEnd);
-                                var priceStart = ProductPromotionText.indexOf("£");
-                                var sub = ProductPromotionText.slice(priceStart+1,ProductPromotionText.length);
-                                var priceEnd = sub.indexOf(" ");
-                                var final = sub.slice(0,priceEnd);
-                                purchasePrice = final
-                                clubcardPrice = (parseFloat(purchasePrice) / parseFloat(purchaseNumber)).toFixed(2);
-                            }
-                        } else {
-                            promotionType = PROMOTION_TYPE.MEAL_DEAL
-                            clubcardPrice = price;
-                            showDeal = false;
-                        }
-                    }
-                    else
-                    {
-                        promotionType = PROMOTION_TYPE.PER_KG;
-                        clubcardPrice = price;
-                        showDeal = false;
-                    }
-                } else {
-                    //Todo Any 3 for 2 Clubcard Price - Cheapest Product Free - Selected Vegetables 80g - 800g
-                    promotionType = PROMOTION_TYPE.CHEAPEST_FREE;
-                    clubcardPrice = price;
-                    showDeal = false;
-                }
-            } else {
-                //Buy 2 or more Save 25% Clubcard Price'
-                promotionType = PROMOTION_TYPE.MULTIBUY_PERCENT;
-                clubcardPrice = price;
-                temp = ProductPromotionText.indexOf("Clubcard Price");
-                var countStart = 4;
-                var countEnd = ProductPromotionText.indexOf("or"); 
-                purchaseNumber = ProductPromotionText.substring(countStart,countEnd);
-                console.log(ProductPromotionText);
-                console.log('purchase number = ',purchaseNumber)
-                var percentStart = ProductPromotionText.indexOf("Save");
-                var percentEnd = ProductPromotionText.indexOf("%")
-                var percent = ProductPromotionText.slice(percentStart+5,percentEnd);
-                console.log('percent=',percent)
-                console.log('purchasePrice ',(parseFloat(price)))
-                clubcardPrice = (parseFloat(price) * ((100.0 - parseFloat(percent))/100.0)).toFixed(2);
-                console.log(clubcardPrice);
-            }
-
-            //calculate effective discount %
-            var effectivePercentageReduction = ((1.00 - (parseFloat(clubcardPrice) / parseFloat(price))).toFixed(2))*100;
-            
             if (ProductPromotionDate != '') {
                 ProductPromotionStart = ProductPromotionDate.slice(30,40);
                 ProductPromotionEnd = ProductPromotionDate.slice(46,57);
                 //Offer valid for delivery from 03/01/2022 until 08/02/2022
             }
 
-            
             var article = {
                 "productId": productId,
                 "title": title,
@@ -249,15 +391,12 @@ async function processNextEntry() {
                 "isAvaialble" : isAvailable,
                 "price": price,
                 "aldiPriceMatch" : AldiPriceMatch,
-                "hasPromotion": hasPromotion,
-                "promotionType": promotionType,
                 "productPromotionText": ProductPromotionText,
                 "productPromotionDate": ProductPromotionDate,
                 "productPromotionStart": ProductPromotionStart,
                 "productPromotionEnd" : ProductPromotionEnd,
-                "clubcardPrice": clubcardPrice,
-                "effectivePercentReduction": effectivePercentageReduction 
             };
+            processCategory(article);
             articles.push(article);
             
         })
